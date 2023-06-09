ef2722a4f388a1c4e9a58ca452f79fd2dbd1e410
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-09 19:29:06 +0900

    petdogの愛犬のプロフィールを2匹に修正

--- src/main/java/prpet/MainDogBreed.java
+++ src/main/java/prpet/MainDogBreed.java
@@ -4,9 +4,7 @@ public class MainDogBreed {
     public static void main(String[] args) {
         Pet firstPet = new Pet("ALBA", "chihuahua", 16);
         Pet secondPet = new Pet("VANILLA", "chihuahua", 5);
-        Pet thirdPet = new Pet("LEMON", "chihuahua", 1);
         firstPet.displayProfile();
         secondPet.displayProfile();
-        thirdPet.displayProfile();
     }
 }
